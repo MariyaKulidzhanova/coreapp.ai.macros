65e27f4cbf395dcab718162749f28a2668e7e986
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2024-12-31 17:08:25 +0300

    Specify point 6: implement rotation which also changes velocity vector

---
+++ CoreApp.Ai.Macros/Commands/RotateAndModifyVelocityCommand.cs
@@ -0,0 +1,27 @@
+﻿using CoreApp.Ai.Macros.Models;
+
+namespace CoreApp.Ai.Macros.Commands
+{
+    public class RotateAndModifyVelocityCommand : ICommand
+    {
+        private readonly UObject _uObject;
+        private readonly int _angle;
+
+        public RotateAndModifyVelocityCommand(UObject uObject, int angle)
+        {
+            _uObject = uObject;
+            _angle = angle;
+        }
+
+        public void Execute()
+        {
+            if (_uObject.Parameters.ContainsKey("velocity"))
+            {
+                int velocity = _uObject.Parameters["velocity"];
+                _uObject.Parameters["velocity"] = velocity; 
+            }
+
+            _uObject.Parameters["angle"] = _angle;
+        }
+    }
+}
